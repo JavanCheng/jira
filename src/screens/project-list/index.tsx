3bcf08fa46a015fd0f11496dd2c6ad64fe6e9c59
Chengyf <1351416566@qq.com>
2021-12-30 19:38:28 +0800

    style: 优化项目列表页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import * as qs from "qs";
 import { cleanObject, useDebounce, useMount } from "utils";
 import { useHttp } from "utils/http";
+import styled from "@emotion/styled";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -32,9 +33,14 @@ export const ProjectListScreen = () => {
   });
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
       <List users={users} list={list} />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,6 @@
-import { Input, Select } from "antd";
+/** @jsxImportSource @emotion/react */
+import { jsx } from "@emotion/react";
+import { Form, Input, Select } from "antd";
 
 export interface User {
   id: string;
@@ -22,12 +24,13 @@ interface SearchPanelProps {
 
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   return (
-    <form>
-      <div>
+    <Form css={{ marginBottom: "2rem" }} layout={"inline"}>
+      <Form.Item>
         {/* 当value的值发生改变时，setParam更新params的值
             首先扩展运算符将原始params保留
             然后evt.target.value获取改变的evt对应type的value */}
         <Input
+          placeholder={"项目名"}
           type="text"
           value={param.name}
           onChange={(evt) =>
@@ -37,6 +40,8 @@ export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
             })
           }
         />
+      </Form.Item>
+      <Form.Item>
         <Select
           value={param.personId}
           onChange={(value) =>
@@ -53,7 +58,7 @@ export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
             </Select.Option>
           ))}
         </Select>
-      </div>
-    </form>
+      </Form.Item>
+    </Form>
   );
 };
