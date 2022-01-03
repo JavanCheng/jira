b0946fa16f9b501d3016cdb36f48760f5433f87e
Chengyf <1351416566@qq.com>
2022-01-03 20:32:46 +0800

    feat: 用useAsync处理loading和error状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,43 +1,30 @@
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import { useState, useEffect } from "react";
-import * as qs from "qs";
-import { cleanObject, useDebounce, useMount } from "utils";
-import { useHttp } from "utils/http";
+import { useState } from "react";
+import { useDebounce } from "utils";
 import styled from "@emotion/styled";
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
+import { Typography } from "antd";
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
-
   // useState第一个参数表示内部当前状态值，第二个参数表示更新状态值的函数
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
   const debouncedParam = useDebounce(param, 200);
-  const [list, setList] = useState([]);
-  const client = useHttp();
-
-  // useEffect：第一个参数返回函数的回调相当于componentWillUnmount
-  // 第二个参数这里是[params],表示监听params的内容
-  // 当params发生改变时，调用一次回调
-  useEffect(() => {
-    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedParam]);
-
-  // 初始化 users
-  useMount(() => {
-    client("users").then(setUsers);
-  });
+  const { isLoading, error, data: list } = useProjects(debouncedParam);
+  const { data: users } = useUsers();
 
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel users={users} param={param} setParam={setParam} />
-      <List users={users} list={list} />
+      <SearchPanel users={users || []} param={param} setParam={setParam} />
+      {error ? (
+        <Typography.Text type={"danger"}>{error.message}</Typography.Text>
+      ) : null}
+      <List loading={isLoading} users={users || []} dataSource={list || []} />
     </Container>
   );
 };
