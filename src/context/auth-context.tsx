f31892238128feffa831b4cca6c347c778ec87c4
Chengyf <1351416566@qq.com>
2022-01-03 21:48:50 +0800

    feat: 处理me接口loading和error

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
 import * as auth from "auth-provider";
 import { User } from "screens/project-list/search-panel";
 import { http } from "utils/http";
 import { useMount } from "utils";
+import { useAsync } from "utils/use-async";
+import { FullPageLoading, FullPageErrorFallback } from "components/lib";
 
 interface AuthForm {
   username: string;
@@ -34,8 +36,16 @@ AuthContext.displayName = "AuthContext";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   // 设置 User 的泛型为 User 类型或 null
-  //   初始化 user 就是 null
-  const [user, setUser] = useState<User | null>(null);
+  // 初始化 user 就是 null
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
   // point free
   // 类似于消参，将 then 里的 user 简化掉
   // 此时 user 会报错，因为类型推断认为我们的类型是 null，所以在创建 user 时声明泛型
@@ -47,8 +57,16 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   //   在页面初始化时调用这个初始化函数
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
   });
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
 
   // 此时 value 会报错，因为类型推断认为我们的类型是 undefined，所以在创建 context 时声明泛型
   return (
