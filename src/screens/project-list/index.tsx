542589e990763b182c9c9d3e7083c3ada8ddcddc
Chengyf <1351416566@qq.com>
2022-01-04 20:48:36 +0800

    feat: 用useUrlQueryParam进行url状态管理

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -6,13 +6,12 @@ import styled from "@emotion/styled";
 import { useProjects } from "utils/project";
 import { useUsers } from "utils/user";
 import { Typography } from "antd";
+import { useUrlQueryParam } from "utils/url";
 
 export const ProjectListScreen = () => {
-  // useState第一个参数表示内部当前状态值，第二个参数表示更新状态值的函数
-  const [param, setParam] = useState({
-    name: "",
-    personId: "",
-  });
+  // 基本类型，可以放到依赖里；组件状态，可以放到依赖里；非组件状态的对象，绝不可以放到依赖里
+  // https://codesandbox.io/s/keen-wave-tlz9s?file=/src/App.js
+  const [param, setParam] = useUrlQueryParam(["name", "personId"]);
   const debouncedParam = useDebounce(param, 200);
   const { isLoading, error, data: list } = useProjects(debouncedParam);
   const { data: users } = useUsers();
@@ -30,6 +29,8 @@ export const ProjectListScreen = () => {
     </Container>
   );
 };
+
+ProjectListScreen.whyDidYouRender = false;
 
 const Container = styled.div`
   padding: 3.2rem;
